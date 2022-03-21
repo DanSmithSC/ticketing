338892f468580d749312b3e5d0b643e45c327cdb
Dan Smith <denzik@gmail.com>
2022-03-21 17:35:11 -0400

    updated startup message - testing deployment

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import { app } from './app'
 
 const start = async () => {
-  console.log('Starting Up - Auth Service.')
+  console.log('Starting Up - Auth Service...')
 
   if(!process.env.JWT_KEY){
     throw new Error('JWT_KEY must be defined.')
